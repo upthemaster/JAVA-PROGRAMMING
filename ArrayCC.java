4e403b4e9b57c35d04720193b20af603665a5a61
upthemaster <upthemaster@gmail.com>
2025-09-03 23:20:22 +0530

    Input/Output/UpdateOperations

--- ArrayCC.java
+++ ArrayCC.java
@@ -4,17 +4,21 @@ public class ArrayCC{
         //Declaration of Array
         int marks[] = new int[50];
         Scanner sc = new Scanner(System.in);
+        
         //Input
         marks[0] = sc.nextInt();
         marks[1] = sc.nextInt();
         marks[2] = sc.nextInt();
+        
         //Output
         System.out.println("P: "+ marks[0]);
         System.out.println("C: "+ marks[1]);
         System.out.println("M: "+ marks[2]);
+        
         //Update
         marks[2] = marks[2]+1;
         System.out.println("M: "+ marks[2]);
+        
         //Percentage
         int percentage = (marks[0]+marks[1]+marks[2])/3;
         System.out.println("Percentage is: "+percentage);
